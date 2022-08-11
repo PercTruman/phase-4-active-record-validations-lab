2c01b36d44d23ecaf6c83cf97f0e6ddb0d4621ea
PercTruman <drshawnhart@gmail.com>
2022-08-11 17:25:34 -0500

    commit tree 86cb3ea53f338e1eb0f14663c54aa68c245a17b0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content,  length: {minimum: 250}
+    validates :summary,  length: {maximum: 250}
+    validates :category, inclusion: { in:(["Fiction", "Non-Fiction"])}
+
+    validate :title_is_clickbait
+
+
+    def title_is_clickbait
+        if  title
+        unless title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top 5")|| title.include?("Guess")
+            errors.add(:title, "Sorry, not clickbaity enough")
+        end
+    end
+        
+    end
 end
